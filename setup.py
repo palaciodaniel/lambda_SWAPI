ea445c13f4b15e4d30b2037789fcbf683358c786
Daniel Palacio <45110344+palaciodaniel@users.noreply.github.com>
2020-09-03 22:17:06 -0300

    Corrected critical typing mistake

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name="lambda_swapi",
       description="A small program to determine in which movies two selected Star Wars characters appear",
       author="Daniel Palacio",
       packages=["packages", "tests"],
-      install_requires=["python>=3.6,
+      install_requires=["python>=3.7.4",
                         "fuzzywuzzy"])
