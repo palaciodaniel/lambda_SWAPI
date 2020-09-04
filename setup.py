ed5b9cf06e75948056b46b4f26f69d6af3ae4040
Daniel Palacio <45110344+palaciodaniel@users.noreply.github.com>
2020-09-03 21:21:09 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name="lambda_swapi",
+      version="2.0.0",
+      description="A small program to determine in which movies two selected Star Wars characters appear",
+      author="Daniel Palacio")
