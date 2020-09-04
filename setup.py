c5b2a6336ef93ba65dc6d5609226fa5ab7ca130e
Daniel Palacio <45110344+palaciodaniel@users.noreply.github.com>
2020-09-03 21:30:15 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ from setuptools import setup
 setup(name="lambda_swapi",
       version="2.0.0",
       description="A small program to determine in which movies two selected Star Wars characters appear",
-      author="Daniel Palacio")
+      author="Daniel Palacio",
+      packages=["packages", "tests"],
+      install_requires=["python>=3.6,
+                        "fuzzywuzzy"])
